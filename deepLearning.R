04582d683fd65fa7590aaccf43238a6c300343cc
Alexey Kutin <alexey.kutin@veeva.com>
2016-06-09 10:23:12 -0400

    initial cut with h2O and deep learning

---
+++ deepLearning.R
@@ -0,0 +1,33 @@
+install.packages("h2o")
+library(h2o)
+
+train <- read.csv("input/train.csv")
+
+h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, nthreads = -1)
+
+set.seed(2909)
+product.sample <- sample(1:nrow(train), 50000) # 80/20 split
+product.train <- train[product.sample, 2:95]
+product.test <- train[-product.sample, 2:95]
+product.check <- train[-product.sample, 95]
+
+product.h2o.train <- as.h2o(product.train)
+product.model <- h2o.deeplearning(x = 1:93,  # column numbers for predictors
+                   y = 94,   # column number for label
+                   training_frame = product.h2o.train, # data in H2O format
+                   activation = "TanhWithDropout", # or 'Tanh'
+                   input_dropout_ratio = 0.01, # % of inputs dropout
+                   hidden_dropout_ratios = c(0.02,0.02,0.02), # % for nodes dropout
+                   balance_classes = TRUE, 
+                   hidden = c(50,50,50), # three layers of 50 nodes
+                   epochs = 100) # max. no. of epochs
+
+product.h2o.test <- as.h2o(product.test)
+product.predict.train <- h2o.predict(product.model, product.h2o.train)
+product.predict.test <- h2o.predict(product.model, product.h2o.test)
+
+product.model # print model statistics
+
+h2o.shutdown(prompt = F)
+
+# plotting for quick visual validation
